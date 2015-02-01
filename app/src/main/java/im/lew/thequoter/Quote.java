1eb0c9094d369f6596662316f295623b8f58643e
Lewis Lebentz <lewislebentz@Lewiss-MacBook-Pro.local>
2015-02-01 15:35:20 +0000

    Initial commit

---
+++ app/src/main/java/im/lew/thequoter/Quote.java
@@ -0,0 +1,26 @@
+package im.lew.thequoter;
+
+/**
+ * Created by lewislebentz on 31/01/2015.
+ */
+public class Quote {
+    public String quote;
+    public String person;
+
+
+    public Quote(String mQuote, String mPerson){
+        super();
+
+        quote = mQuote;
+        person = mPerson;
+
+    }
+
+    public String getPerson() {
+        return person;
+    }
+
+    public String getQuote() {
+        return quote;
+    }
+}
